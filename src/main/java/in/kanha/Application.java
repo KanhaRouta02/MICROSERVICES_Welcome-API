c4e1f375de09093ae8371a075d873f2e27e4f9a6
Kanha Routa <147900871+KanhaRouta02@users.noreply.github.com>
2024-06-23 21:39:48 +0530

    commit

--- src/main/java/in/kanha/Application.java
+++ src/main/java/in/kanha/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+  // 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
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
